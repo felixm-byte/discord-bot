dc647010c90e0f34030aa7b653c6b430af78febb
felixexists <72092800+felixm-byte@users.noreply.github.com>
2024-08-22 11:17:18 +0100

    Prioritised more relevant queries

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import discord
 from discord.ext import commands
 import requests
-
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -16,10 +15,24 @@ async def on_ready():
 
 @client.hybrid_command()
 async def search(ctx, arg):
-    arg = arg.lower()
-    res = requests.get(f"https://www.googleapis.com/customsearch/v1?key=&cx=a48e7f7f7b07c44ca&q={arg}")
-    print(res.text)
-    res = res.json()
-    await ctx.send(f"Result: {res["items"][0]["title"]} \nFrom: {res["items"][0]["displayLink"]} \nLink: {res["items"][0]["link"]}")
+    try:
+        arg = arg.lower()
+        res = requests.get(f"https://www.googleapis.com/customsearch/v1?key=&cx=a48e7f7f7b07c44ca&q={arg}")
+        print(res.text)
+        res = res.json()
+        if not ('items' in res.keys()) or res["items"] == []:
+            res = requests.get(f"https://www.googleapis.com/customsearch/v1?key=&cx=219860434a93544f2&q={arg}")
+            print(res.text)
+            res = res.json()
+            if res["items"] == []:
+                await ctx.send(f"No results found for {arg}, please try again with a different search term.")
+            else:
+                await ctx.send(f"Result: {res["items"][0]["title"]} \nFrom: {res["items"][0]["displayLink"]} \nLink: {res["items"][0]["link"]}")
+        else:
+            await ctx.send(f"Result: {res["items"][0]["title"]} \nFrom: {res["items"][0]["displayLink"]} \nLink: {res["items"][0]["link"]}")
+    except Exception as e:
+        await ctx.send(f"An unexpected error occured. Try using a different search query, or share this code with an admin if that doesn't work: \n{e}")
+
+
 
 client.run(DISCORD_TOKEN)
